dffb8e5ae16d66932211cc506c1881ad7b8b1b44
Marius Niklaus <m.n.niklaus.97@gmail.com>
2023-04-18 08:48:37 +0200

    merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,25 @@
-'use client'
+'use client';
 
-import Image from 'next/image'
-import {Inter} from 'next/font/google'
-import {Session} from "next-auth";
-import {SessionProvider, signIn} from "next-auth/react";
-import {getBody} from "next-auth/next/utils";
-import {Hero} from "@/components/hero/hero";
-import {signin} from "next-auth/core/routes";
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import { Session } from 'next-auth';
+import { SessionProvider, signIn } from 'next-auth/react';
+import { getBody } from 'next-auth/next/utils';
+import { Hero } from '@/components/hero/hero';
+import { signin } from 'next-auth/core/routes';
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ['latin'] });
 
 export interface HomePageProps {
-    session: Session
+  session: Session;
 }
 
 export default function Home() {
+  const handler = {
+    onLogin: () => {
+      signIn('google', { callbackUrl: '/dashboard' });
+    },
+  };
 
-    const handler = {
-        onLogin: () => {
-            signIn('google', {callbackUrl: '/dashboard'})
-        }
-    }
-
-    return (
-        <Hero onClickHandler={handler.onLogin}></Hero>
-    )
+  return <Hero onClickHandler={handler.onLogin}></Hero>;
 }
