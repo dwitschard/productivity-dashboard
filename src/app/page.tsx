f94a3c92298b93b1039bd17197b9586bfd76e813
Dominik Witschard <dominik.witschard@zuehlke.com>
2023-04-18 10:45:15 +0200

    cleanup: removed unused components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,9 @@
 'use client';
 
-import { Inter } from 'next/font/google';
-import { Session } from 'next-auth';
-import { signIn, useSession } from 'next-auth/react';
+import { signIn } from 'next-auth/react';
 import { Hero } from '@/components/hero/hero';
-import { useRouter } from 'next/navigation';
-import { Spinner } from '@/components/spinner/spinner';
-
-const inter = Inter({ subsets: ['latin'] });
-
-export interface HomePageProps {
-  session: Session;
-}
 
 export default function Home() {
-  const router = useRouter();
-
   const handler = {
     onLogin: () => {
       signIn('google', { callbackUrl: '/dashboard' });
