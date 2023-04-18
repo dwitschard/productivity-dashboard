5f20ff74d9e4a37bb36269e7c925fb5262d20071
Marius Niklaus <m.n.niklaus.97@gmail.com>
2023-04-18 11:46:14 +0200

    basic structure

--- src/components/helpers/helpers.tsx
+++ src/components/helpers/helpers.tsx
@@ -1,10 +1,17 @@
+'use client'
 import { FC } from 'react';
-import { HeroProps } from '@/components/landing-page/hero/hero';
 
 const Helpers: FC<{}> = () => {
   return (
     <div>
-      <div>Helpers</div>
+      <h2>Helper Tools</h2>
+      <input type="text" placeholder="Search" className='' onChange={(e)=>console.log(e.target.value)}/>
+      <div className='grid grid-cols-2 gap-4'>
+        <div className='bg-gray-200 p-4 rounded-lg'>Hex Decimal</div>
+        <div className='bg-gray-200 p-4 rounded-lg'>Json validator</div>  
+        <div className='bg-gray-200 p-4 rounded-lg'>Regex matcher</div>  
+        <div className='bg-gray-200 p-4 rounded-lg'>Date lookup</div>
+      </div>
     </div>
   );
 };
