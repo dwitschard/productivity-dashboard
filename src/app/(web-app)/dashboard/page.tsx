1f01802cdcb3c0088184ca93ebed29e9b4d9dd27
lombold <colin.disler@gmail.com>
2023-04-20 10:41:33 +0200

    reorder dashboard widgets

--- src/app/(web-app)/dashboard/page.tsx
+++ src/app/(web-app)/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import News from '@/components/news/news';
 import CodePlaygroundWidget from '@/components/code-playground/code-playground-widget';
 import HelperTools from '@/components/helper-tools/helper-tools';
 import PasteBinContainer from '@/components/paste-bin/paste-bin.container';
@@ -8,9 +7,8 @@ export default function DashboardPage() {
     <div className="py-8 px-9">
       <h1 className="text-3xl leading-9 font-bold mb-2.5">Tabsly</h1>
       <div className="grid grid-cols-2 grid-rows-2 min-h-screen gap-8">
-        <News />
-        <PasteBinContainer />
         <CodePlaygroundWidget />
+        <PasteBinContainer />
         <HelperTools />
       </div>
     </div>
