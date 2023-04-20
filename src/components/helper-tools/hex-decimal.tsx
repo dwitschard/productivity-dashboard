4b94ae482a6b59f971a87ee6226e5f6b33bf68ad
Dominik Witschard <dominik.witschard@zuehlke.com>
2023-04-20 14:32:27 +0200

    hex-converter: added copy button

--- src/components/helper-tools/hex-decimal.tsx
+++ src/components/helper-tools/hex-decimal.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { FC, useState } from 'react';
+import { ButtonInputField } from '@/components/shared/button-input-field/button-input-field';
 
 const HexDecimal: FC<{}> = () => {
   const [dec, setDec] = useState('');
@@ -9,6 +10,10 @@ const HexDecimal: FC<{}> = () => {
 
   function calculate(value: string, format: string): void {
     let decimal = parseInt(value).toString();
+    if (isNaN(+decimal) || !decimal) {
+      decimal = '0';
+    }
+
     switch (format) {
       case 'hex':
         decimal = parseInt(value, 16).toString();
@@ -30,43 +35,55 @@ const HexDecimal: FC<{}> = () => {
     <div className="grid grid-cols-2 gap-8  w-full">
       <div>
         <h3 className="text-sm leading-5 mb-1 font-medium">Decimal</h3>
-        <input
-          type="text"
-          placeholder="6872"
-          className="px-2 py-3 w-full shadow-sm rounded border border-gray-300"
-          value={dec}
-          onChange={(e) => calculate(e.target.value, 'dec')}
-        />
+        <div className={'flex w-full relative isolate'}>
+          <input
+            type="text"
+            placeholder="6872"
+            className="px-2 py-3 w-full shadow-sm rounded border border-gray-300"
+            value={dec}
+            onChange={(e) => calculate(e.target.value, 'dec')}
+          />
+          <ButtonInputField disabled={!dec} onCopy={() => navigator.clipboard.writeText(dec)} />
+        </div>
       </div>
       <div>
         <h3 className="text-sm leading-5 mb-1 font-medium">Hexadecimal</h3>
-        <input
-          type="text"
-          placeholder="1ad8"
-          className="px-2 py-3 w-full shadow-sm rounded border border-gray-300"
-          value={hex}
-          onChange={(e) => calculate(e.target.value, 'hex')}
-        />
+        <div className="flex w-full relative isolate">
+          <input
+            type="text"
+            placeholder="1ad8"
+            className="px-2 py-3 pr-10 w-full shadow-sm rounded border border-gray-300"
+            value={hex}
+            onChange={(e) => calculate(e.target.value, 'hex')}
+          />
+          <ButtonInputField disabled={!hex} onCopy={() => navigator.clipboard.writeText(hex)} />
+        </div>
       </div>
       <div>
         <h3 className="text-sm leading-5 mb-1 font-medium">Octal</h3>
-        <input
-          type="text"
-          placeholder="15330"
-          className="px-2 py-3 w-full shadow-sm rounded border border-gray-300"
-          value={oct}
-          onChange={(e) => calculate(e.target.value, 'oct')}
-        />
+        <div className={'flex w-full relative isolate'}>
+          <input
+            type="text"
+            placeholder="15330"
+            className="px-2 py-3 w-full shadow-sm rounded border border-gray-300"
+            value={oct}
+            onChange={(e) => calculate(e.target.value, 'oct')}
+          />
+          <ButtonInputField disabled={!oct} onCopy={() => navigator.clipboard.writeText(oct)} />
+        </div>
       </div>
       <div>
         <h3 className="text-sm leading-5 mb-1 font-medium">Binary</h3>
-        <input
-          type="text"
-          placeholder="1101011011000"
-          className="px-2 py-3 w-full shadow-sm rounded border border-gray-300"
-          value={bin}
-          onChange={(e) => calculate(e.target.value, 'bin')}
-        />
+        <div className={'flex w-full relative isolate'}>
+          <input
+            type="text"
+            placeholder="1101011011000"
+            className="px-2 py-3 w-full shadow-sm rounded border border-gray-300"
+            value={bin}
+            onChange={(e) => calculate(e.target.value, 'bin')}
+          />
+          <ButtonInputField disabled={!bin} onCopy={() => navigator.clipboard.writeText(bin)} />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/shared/copy-button/copy-button.component.tsx
@@ -0,0 +1,38 @@
+import { ClipboardDocumentIcon } from '@heroicons/react/24/outline';
+import { FC, useState } from 'react';
+export interface CopyButtonProps {
+  onCopy: () => void;
+  disabled?: boolean;
+  className?: string;
+}
+
+export const CopyButton: FC<CopyButtonProps> = ({
+  onCopy,
+  className,
+  disabled
+}: CopyButtonProps) => {
+  const [showCopyLabel, setShowCopyLabel] = useState(false);
+
+  return (
+    <button
+      disabled={disabled}
+      className={
+        className + ' disabled:bg-gray-300 bg-blue-500 hover:bg-blue-600 rounded border-0 p-2'
+      }
+      onClick={() => {
+        onCopy();
+        setShowCopyLabel(true);
+        setTimeout(() => {
+          setShowCopyLabel(false);
+        }, 500);
+      }}>
+      {showCopyLabel && (
+        <div className={'bg-black absolute -mt-12 -ml-5 rounded p-1 text-xs text-white'}>
+          Copied!
+        </div>
+      )}
+
+      <ClipboardDocumentIcon className={'w-4 h-4 text-white'} />
+    </button>
+  );
+};
