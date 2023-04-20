e9d98e2b7e8df025fa4ad53c845148034867e042
Marius Niklaus <m.n.niklaus.97@gmail.com>
2023-04-20 11:45:24 +0200

    figma design implementation

--- src/components/helper-tools/helper-tools.tsx
+++ src/components/helper-tools/helper-tools.tsx
@@ -53,7 +53,7 @@ const HelperTools: FC<{}> = () => {
       {expanded === '' && (
         <>
           <SearchInput onInput={filterTools}></SearchInput>
-          <div className={'grid grid-cols-2 gap-4'}>
+          <div className={'w-full grid grid-cols-2 lg:grid-cols-3 gap-4'}>
             {tools.map((name) => (
               <button
                 key={name}
--- src/components/helper-tools/hex-decimal.tsx
+++ src/components/helper-tools/hex-decimal.tsx
@@ -2,36 +2,72 @@
 import { FC, useState } from 'react';
 
 const HexDecimal: FC<{}> = () => {
-  const [hex, setHex] = useState('');
   const [dec, setDec] = useState('');
+  const [hex, setHex] = useState('');
+  const [oct, setOct] = useState('');
+  const [bin, setBin] = useState('');
 
-  function calculate(hex: string, dec: string): void {
-    if (!hex.match(/^(\s*|[0-9a-fA-F]+)$/) || !dec.match(/^(\s*|[\d]+)$/)) return;
-    setDec(dec);
-    setHex(hex);
-    if (hex !== '') {
-      setDec(parseInt(hex, 16).toString());
-    } else if (dec !== '') {
-      setHex(parseInt(dec, 10).toString(16));
+  function calculate(value: string, format: string): void {
+    let decimal = parseInt(value).toString();
+    switch (format) {
+      case 'hex':
+        decimal = parseInt(value, 16).toString();
+        break;
+      case 'oct':
+        decimal = parseInt(value, 8).toString();
+        break;
+      case 'bin':
+        decimal = parseInt(value, 2).toString();
+        break;
     }
+    setDec(decimal);
+    setHex(parseInt(decimal).toString(16));
+    setOct(parseInt(decimal).toString(8));
+    setBin(parseInt(decimal).toString(2));
   }
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Hexadecimal"
-        className="w-full"
-        value={hex}
-        onChange={(e) => calculate(e.target.value, '')}
-      />
-      <input
-        type="text"
-        placeholder="Decimal"
-        className="w-full"
-        value={dec}
-        onChange={(e) => calculate('', e.target.value)}
-      />
+    <div className="grid grid-cols-2 gap-8  w-full">
+      <div>
+        <h3 className="text-sm leading-5 mb-1 font-medium">Decimal</h3>
+        <input
+          type="text"
+          placeholder="6872"
+          className="px-2 py-3 w-full shadow-sm rounded border border-gray-300"
+          value={dec}
+          onChange={(e) => calculate(e.target.value, 'dec')}
+        />
+      </div>
+      <div>
+        <h3 className="text-sm leading-5 mb-1 font-medium">Hexadecimal</h3>
+        <input
+          type="text"
+          placeholder="1ad8"
+          className="px-2 py-3 w-full shadow-sm rounded border border-gray-300"
+          value={hex}
+          onChange={(e) => calculate(e.target.value, 'hex')}
+        />
+      </div>
+      <div>
+        <h3 className="text-sm leading-5 mb-1 font-medium">Octal</h3>
+        <input
+          type="text"
+          placeholder="15330"
+          className="px-2 py-3 w-full shadow-sm rounded border border-gray-300"
+          value={oct}
+          onChange={(e) => calculate(e.target.value, 'oct')}
+        />
+      </div>
+      <div>
+        <h3 className="text-sm leading-5 mb-1 font-medium">Binary</h3>
+        <input
+          type="text"
+          placeholder="1101011011000"
+          className="px-2 py-3 w-full shadow-sm rounded border border-gray-300"
+          value={bin}
+          onChange={(e) => calculate(e.target.value, 'bin')}
+        />
+      </div>
     </div>
   );
 };
